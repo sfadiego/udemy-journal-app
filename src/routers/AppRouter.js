9750142343c31268282648a6532dbed233263356
diego silva <sfadiego@gmail.com>
2021-04-22 16:31:49 -0500

    login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { firebase } from "../firebase/firebase-config";
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,8 +8,34 @@ import {
 } from "react-router-dom";
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouters } from './AuthRouters'
+import { useDispatch } from 'react-redux';
+import { login } from '../components/actions/auth';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch()
+    const [checking, setChecking] = useState(true)
+    const [isloggedIn, setIsloggedIn] = useState(false)
+    useEffect(() => {
+        // regresa un observador, que vigila el estado de la session
+        firebase
+            .auth()
+            .onAuthStateChanged((user) => {
+                if (user?.uid) {
+                    setIsloggedIn(true)
+                    dispatch(login(user.uid, user.displayName))
+                }else{
+                    setIsloggedIn(false)
+                }
+            })
+        setChecking(false)
+
+    }, [dispatch, checking])
+
+    if(checking){
+        return (
+            <h1>Cargando ...</h1>
+        )
+    }
     return (
         <Router>
             <Switch>
